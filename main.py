e0f49bab8d4b28524f4c4b5c7dcd412fb334fb31
Ritvik Mishra <51820183+Jade9ja@users.noreply.github.com>
2020-05-18 20:36:39 +0530

    Update to how it gets deployed

--- main.py
+++ main.py
@@ -2,24 +2,15 @@ import discord
 from discord.ext import commands
 import io
 import os
-import aiohttp
-BOT_TOKEN = '<Your bot token here>'
+from utils import default
 client = commands.Bot(command_prefix = '/')
-
+config = default.get("config.json")
 @client.event
 async def on_ready():
-    print('ready!')
-
-@client.command()
-async def load(ctx,extension):
-    client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def unload(ctx,extension):
-    client.unload_extension(f'cogs.{extension}')
-
+    print('We ready fam!')
+    
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('BOT_TOKEN')
+client.run(config.token)
